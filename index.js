ddf9a42501bf176fe814914aec5047be98e192c7
dylan <runnels.dylan@yahoo.com>
2024-05-22 13:37:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
+let x = 2500;
 function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return 'This one is on me!';
-  } else if (distance <= 2000) {
+
+  } else if (distance > 400 && distance <= 2000) {
+
     return 'That will be twenty bucks.';
-  } else if (distance <= 2500) {
+
+  } else if (distance > 2000 && distance <= 2500) {
+
     return 'I will gladly take your thirty bucks.';
+
   } else {
     return 'No can do.';
   }
 }
 
+
+console.log(scuberGreetingForFeet(x));
+
+let city = 'NYC';
+city = 'Pittsburgh';
 function ternaryCheckCity(city) {
-  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
-function switchOnCharmFromTip(tip) {
-  switch (tip) {
-    case 'generous':
-      return 'Thank you so much.';
-    case 'not as generous':
-      return 'Thank you.';
-    default:
-      return 'Bye.';
-  }
+console.log(ternaryCheckCity(city));
+let tips= 'generous';
+function switchOnCharmFromTip(tips) {
+switch(tips) 
+{
+  case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous':
+   return 'Thank you.';
+  default:
+  return 'Bye.';
 }
+
+}
+console.log(switchOnCharmFromTip(tips));
